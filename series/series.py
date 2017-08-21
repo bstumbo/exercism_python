52de31fcfb6b4341d519da3f1a46d59d70c5835f
Brandon Stumbo <bstumbo@gwu.edu>
2017-08-21 12:55:25 -0500

    Series kata;
    Signed-off-by: Brandon Stumbo <bstumbo@gwu.edu>

--- series/series.py
+++ series/series.py
@@ -1,2 +1,19 @@
-def slices():
-    pass
+def slices(nums, digits):
+    list_build = []
+    answer = []
+
+    for i, v in enumerate(list(nums)):
+        if len(list(nums[i:i + digits])) == digits:
+            list_build.append(list(nums[i:i + digits]))
+            
+    for item in list_build:
+        answer.append(list(map(int, item)))
+    
+    if not answer:
+        raise ValueError("Oh no, something went wrong!")
+    
+    for x in answer:
+        if len(x) == 0:
+            raise ValueError("Busted")
+         
+    return answer
